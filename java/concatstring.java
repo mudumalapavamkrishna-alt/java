0a360e140b5993e548f54a0d350b8e1c3244c56c
mudumalapavamkrishna-alt <mudumalapavamkrishna@gmail.com>
2025-11-28 11:04:46 +0530

    Create concatstring.java

---
+++ java/concatstring.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main (String [] args){
+    String name ="pavan";
+    int age = 21;
+    System.out.println("my name is " + name + " and i am" + age + " years old");
+    }
+}
