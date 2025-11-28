a4425d41de1cd22c19d05511754ab0e0f05e1da0
mudumalapavamkrishna-alt <mudumalapavamkrishna@gmail.com>
2025-11-28 08:25:04 +0530

    Create operators.java

---
+++ java/operators.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 10;
+    int y = 3;
+    System.out.println(x + y); /
+    System.out.println(x - y); 
+    System.out.println(x * y); 
+    System.out.println(x / y); 
+    System.out.println(x % y); 
+  }
+}
