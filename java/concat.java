12f0cb25069ef88b7b0160d4ff981fcd027ebb89
mudumalapavamkrishna-alt <mudumalapavamkrishna@gmail.com>
2025-11-28 11:23:07 +0530

    Create concat.java

---
+++ java/concat.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main (String [] args){
+    String a ="i am ";
+    String b =" pavan ";
+    String c =" krishna ";
+    String result = a . concat (b). concat(c);
+    
+    System.out.println(result);
+    }
+}
